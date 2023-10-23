518118ef0d851c41d49d5d67e926e40550dc5ab0
piotr <piotrkepistyuwm@gmail.com>
2023-10-23 13:20:59 +0200

    lab3 zad3

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Osoba, Stanowisko
 
+class OsobaAdmin(admin.ModelAdmin):
+    readonly_fields = ('data_dodania',)
+
 admin.site.register(Stanowisko)
-admin.site.register(Osoba)
+admin.site.register(Osoba, OsobaAdmin)
