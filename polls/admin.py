36343899d53187581ba648611ea0661e236dd3e4
piotr <piotrkepistyuwm@gmail.com>
2023-10-23 13:33:37 +0200

    lab3 zad8

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Osoba, Stanowisko
 
+@admin.display(description='Stanowisko (id)')
+def stanowisko_display(obj):
+    return f'{obj.stanowisko.nazwa} ({obj.stanowisko.id})'
+
 class OsobaAdmin(admin.ModelAdmin):
-    list_display = ['imie', 'nazwisko', 'plec', 'stanowisko', 'data_dodania']
+    list_display = ('imie', 'nazwisko', 'plec', 'stanowisko_display', 'data_dodania')  # Uwzględnienie stanowisko_display
+    list_filter = ('stanowisko',)  # Dodanie filtru dla stanowiska
 
 admin.site.register(Stanowisko)
 admin.site.register(Osoba, OsobaAdmin)
