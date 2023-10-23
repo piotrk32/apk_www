c3d73155b3c76699afaf6d31859efb6df7e36865
piotr <piotrkepistyuwm@gmail.com>
2023-10-23 13:26:06 +0200

    lab3 zad5

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,11 @@
-import datetime
 
 from django.db import models
-from django.utils import timezone
-
 
 class GenderChoices(models.IntegerChoices):
     Kobieta = 1
     Mężczyzna = 2
     Inne = 3
+
 
 class Stanowisko(models.Model):
     nazwa = models.CharField(max_length=255, blank=False)
@@ -16,18 +14,13 @@ class Stanowisko(models.Model):
     def __str__(self):
         return self.nazwa
 
+
 class Osoba(models.Model):
-    PLEC_CHOICES = [
-        ('K', 'Kobieta'),
-        ('M', 'Mężczyzna'),
-        ('I', 'Inne'),
-    ]
     imie = models.CharField(max_length=255, blank=False)
     nazwisko = models.CharField(max_length=255, blank=False)
-    plec = models.CharField(max_length=1, choices=PLEC_CHOICES)
+    plec = models.IntegerField(choices=GenderChoices.choices)
     stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE)
     data_dodania = models.DateField(auto_now_add=True)
-    plec = models.IntegerField(choices=GenderChoices.choices)
 
     def __str__(self):
         return f'{self.imie} {self.nazwisko}'
