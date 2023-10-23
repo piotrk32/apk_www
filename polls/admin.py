608c21a77b0661c4b3a070f548e8b52f3f4a349a
piotr <piotrkepistyuwm@gmail.com>
2023-10-23 13:10:19 +0200

    lab3

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Osoba, Stanowisko
 
-# Register your models here.
+admin.site.register(Stanowisko)
+admin.site.register(Osoba)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=255, blank=False)
+    opis = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nazwa
+
+class Osoba(models.Model):
+    PLEC_CHOICES = [
+        ('K', 'Kobieta'),
+        ('M', 'Mężczyzna'),
+        ('I', 'Inne'),
+    ]
+    imie = models.CharField(max_length=255, blank=False)
+    nazwisko = models.CharField(max_length=255, blank=False)
+    plec = models.CharField(max_length=1, choices=PLEC_CHOICES)
+    stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.imie} {self.nazwisko}'
